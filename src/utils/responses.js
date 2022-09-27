30add38c93fda3fc1a27b295f9452fe075d11bf7
Agustin Coelho <coelhoagustin1@gmail.com>
2022-09-27 17:51:43 -0300

    quite comentarios

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -10,30 +10,30 @@ const options = {
 let state = { option: null, waitingName: false }
 
 const handleResponse = async (bot, msg) => {
-  // if (!state.waitingName) {
-  //   switch (msg.text) {
-  //     case options.airing:
-  //       bot.sendMessage(msg.chat.id, 'Decime el nombre de anime pls', { reply_markup: JSON.stringify({ hide_keyboard: true }) })
-  //       state = { option: options.airing, waitingName: true }
-  //       break
+  if (!state.waitingName) {
+    switch (msg.text) {
+      case options.airing:
+        bot.sendMessage(msg.chat.id, 'Decime el nombre de anime pls', { reply_markup: JSON.stringify({ hide_keyboard: true }) })
+        state = { option: options.airing, waitingName: true }
+        break
 
-  //     case options.info:
-  //       bot.sendMessage(msg.chat.id, 'Decime el nombre de anime pls', { reply_markup: JSON.stringify({ hide_keyboard: true }) })
-  //       state = { option: options.info, waitingName: true }
-  //       break
+      case options.info:
+        bot.sendMessage(msg.chat.id, 'Decime el nombre de anime pls', { reply_markup: JSON.stringify({ hide_keyboard: true }) })
+        state = { option: options.info, waitingName: true }
+        break
 
-  //     default:
-  //       bot.sendMessage(msg.chat.id, options.firstResponse, { "reply_markup": { "keyboard": [[options.info], [options.airing]] } })
-  //   }
-  // } else {
-  const data = msg.text
-  const res = await getDataByAnimeName(data)
-  const { text, img } = nextAiringEpisode(res)
-  if (img !== undefined) bot.sendPhoto(msg.chat.id, img)
-  bot.sendMessage(msg.chat.id, text)
-    .then(() => bot.sendMessage(msg.chat.id, '🙌🏻 Espero haberte ayudado!'))
-  return state = { option: null, waitingName: false }
-  //   }
+      default:
+        bot.sendMessage(msg.chat.id, options.firstResponse, { "reply_markup": { "keyboard": [[options.info], [options.airing]] } })
+    }
+  } else {
+    const data = msg.text
+    const res = await getDataByAnimeName(data)
+    const { text, img } = nextAiringEpisode(res)
+    if (img !== undefined) bot.sendPhoto(msg.chat.id, img)
+    bot.sendMessage(msg.chat.id, text)
+      .then(() => bot.sendMessage(msg.chat.id, '🙌🏻 Espero haberte ayudado!'))
+    return state = { option: null, waitingName: false }
+  }
 }
 
 export default handleResponse
